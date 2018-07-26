6de22bfb05a254c83bc31dc03bb6f50b0aad71c2
Tim "Lightning2x" Schagen <schagen34@gmail.com>
2018-07-26 14:20:09 +0200

    quick removal of useless variable (oops)

--- Core/AssetManager.cs
+++ Core/AssetManager.cs
@@ -10,18 +10,21 @@ namespace MGJamSummer2018.Core
     {
         public static AssetManager Instance { get { return instance ?? (instance = new AssetManager()); } }
         private static AssetManager instance;
-        private List<SoundEffect> sfxCache;
         private ContentManager content;
-        public AssetManager()
-        {
-            sfxCache = new List<SoundEffect>();
-        }
 
+        public AssetManager()
+        { }
+
+        // General
         public void SetContentManager(ContentManager _content) { content = _content; }
         public ContentManager GetContentManager { get => content; }
+
+        // Graphics
         public Texture2D GetSprite(string path) { return content.Load<Texture2D>(path); }
         public SpriteFont GetFont(string path) { return content.Load<SpriteFont>(path); }
         public Effect GetShader(string path) { return content.Load<Effect>(path); }
+
+        // Music & SFX
         public void PlaySound(string path) { SoundEffect snd = content.Load<SoundEffect>(path); snd.Play(); }
         public void PlayMusic(string path, bool repeat) { MediaPlayer.IsRepeating = repeat; MediaPlayer.Play(content.Load<Song>(path)); }
         public void MusicVolume(float percent) { percent = percent / 100f; MediaPlayer.Volume = percent; }
