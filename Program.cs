dc2e2f08bcbd2289e74aa5ed6791aff916848de9
Tim "Lightning2x" Schagen <schagen34@gmail.com>
2018-07-26 12:34:01 +0200

    initial .net core

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MGJamSummer2018
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
